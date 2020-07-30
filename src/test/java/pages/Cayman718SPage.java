6cb83ad2dec8eedb6190aed18b1b434e7511b574
Heydar Abdullayev <heydar.abdullayev@gmail.com>
2020-07-30 00:41:15 -0400

    line is deleted

--- src/test/java/pages/Cayman718SPage.java
+++ src/test/java/pages/Cayman718SPage.java
@@ -7,13 +7,9 @@ import utilities.Driver;
 
 public class Cayman718SPage {
 
-
-
     public Cayman718SPage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
-
-
 
     @FindBy(xpath = "//div[@id='model-result-row']/div[3]/div[2]/div[1]/div[2]/div[@class='m-14-model-price']")
     public static WebElement caymanBasePriceBeforeClick;
